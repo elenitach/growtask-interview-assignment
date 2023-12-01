41539f876b62e330b5a0fec62cc1311c1ad8358b
elenitach <lena.01460@gmail.com>
2023-12-02 01:02:18 +0500

    Make a slider and a pictures block

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const swiper = new Swiper(document.querySelector(".photos"), {
+const photosSwiper = new Swiper(".photos", {
+  slidesPerView: "auto",
+  spaceBetween: 16,
+});
+
+const sliderSwiper = new Swiper(".slider__items", {
   slidesPerView: "auto",
   spaceBetween: 16,
 });
