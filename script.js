838e6b0811d161e01337d721e5badd5a01660116
elenitach <lena.01460@gmail.com>
2023-12-04 19:44:04 +0500

    add styles for different states

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const photosSwiper = new Swiper(".photos", {
+new Swiper(".photos", {
   slidesPerView: "auto",
   spaceBetween: 16,
 });
 
-const sliderSwiper = new Swiper(".slider__items", {
+new Swiper(".slider__items", {
   slidesPerView: "auto",
   spaceBetween: 16,
 });
@@ -15,22 +15,17 @@ const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menu__item");
 const buttonToForm = document.querySelector(".menu__button");
 const buttonsSocial = document.querySelectorAll(".menu__social .button");
-const pictures = document.querySelectorAll('.picture');
+const pictures = document.querySelectorAll(".picture");
 const picturesRadios = document.querySelectorAll(".pictures__radio");
+const faqs = document.querySelectorAll(".question");
+const form = document.querySelector(".form");
+const submitButton = form.querySelector(".form__button");
 
 const toggleMenu = () => {
-  if (!buttonMenu.classList.contains("button_menu_open")) {
-    buttonMenu.classList.add("button_menu_open");
-    menu.classList.add("menu_visible");
-    headerLogo.classList.add('hidden');
-    headerText.classList.remove('hidden');
-
-  } else {
-    buttonMenu.classList.remove("button_menu_open");
-    menu.classList.remove("menu_visible");
-    headerLogo.classList.remove("hidden");
-    headerText.classList.add("hidden");
-  }
+  buttonMenu.classList.toggle("button_menu_open");
+  menu.classList.toggle("menu_visible");
+  headerLogo.classList.toggle("hidden");
+  headerText.classList.toggle("hidden");
 };
 
 [buttonMenu, buttonToForm, ...menuItems, ...buttonsSocial].forEach((elem) => {
@@ -39,11 +34,123 @@ const toggleMenu = () => {
 
 const toggleActivePicture = (idx) => {
   picturesRadios.forEach((radio) => radio.classList.remove("radio_active"));
-  picturesRadios[idx].classList.add('radio_active');
+  picturesRadios[idx].classList.add("radio_active");
   pictures.forEach((picture) => picture.classList.remove("picture_active"));
-  pictures[idx].classList.add('picture_active');
-}
+  pictures[idx].classList.add("picture_active");
+};
 
 picturesRadios.forEach((radio, idx) => {
-  radio.addEventListener('click', () => toggleActivePicture(idx))
+  radio.addEventListener("click", () => toggleActivePicture(idx));
+});
+
+faqs.forEach((q) => {
+  const summary = q.querySelector("summary");
+  const answer = q.querySelector(".question__answer");
+  const icon = q.querySelector(".button__img");
+  let isClosing = false;
+  let isExpanding = false;
+  let animation = null;
+
+  const onAnimationFinish = (open) => {
+    q.open = open;
+    animation = null;
+    isClosing = false;
+    isExpanding = false;
+    q.style.height = "";
+  };
+
+  const expand = () => {
+    isExpanding = true;
+    const startHeight = `${q.offsetHeight}px`;
+    const endHeight = `${summary.offsetHeight + answer.offsetHeight}px`;
+
+    if (animation) {
+      animation.icon.cancel();
+      animation.answer.cancel();
+    }
+    animation = {
+      icon: icon.animate(
+        {
+          transform: ["rotate(0)", "rotate(-45deg)"],
+        },
+        {
+          duration: 300,
+          easing: "ease-out",
+        }
+      ),
+      answer: q.animate(
+        {
+          height: [startHeight, endHeight],
+        },
+        {
+          duration: 300,
+          easing: "ease-out",
+        }
+      ),
+    };
+    animation.answer.onfinish = () => onAnimationFinish(true);
+    animation.answer.oncancel = () => (isExpanding = false);
+  };
+
+  const close = () => {
+    isClosing = true;
+    const startHeight = `${q.offsetHeight}px`;
+    const endHeight = `${summary.offsetHeight}px`;
+    if (animation) {
+      animation.icon.cancel();
+      animation.answer.cancel();
+    }
+    animation = {
+      icon: icon.animate(
+        {
+          transform: ["rotate(-45deg)", "rotate(0)"],
+        },
+        {
+          duration: 300,
+          easing: "ease-out",
+        }
+      ),
+      answer: q.animate(
+        {
+          height: [startHeight, endHeight],
+        },
+        {
+          duration: 300,
+          easing: "ease-out",
+        }
+      ),
+    };
+    animation.answer.onfinish = () => onAnimationFinish(false);
+    animation.answer.oncancel = () => (isClosing = false);
+  };
+
+  const open = () => {
+    q.style.height = `${q.offsetHeight}px`;
+    q.open = true;
+    window.requestAnimationFrame(expand);
+  };
+
+  summary.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (isClosing || !q.open) {
+      open();
+    } else if (isExpanding || q.open) {
+      close();
+    }
+  });
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e);
+});
+
+submitButton.addEventListener("click", () => {
+  const invalidInputs = form.querySelectorAll(".form__input:invalid");
+  invalidInputs.forEach((input) => {
+    input.classList.add("input_invalid");
+    input.addEventListener("input", (e) =>
+      e.target.classList.remove("input_invalid")
+    );
+  });
 });
